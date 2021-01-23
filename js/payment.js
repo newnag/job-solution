7dff4805131ab216c5b06496a17785c72754f2a8
newnag <48855584+newnag@users.noreply.github.com>
2021-01-23 17:20:37 +0700

    23/01/2021

---
+++ js/payment.js
@@ -0,0 +1,12 @@
+$("#date").flatpickr({
+  dateFormat: "d-m-Y",
+  disableMobile: "true"
+})
+
+$("#time").flatpickr({
+  enableTime: true,
+  noCalendar: true,
+  dateFormat: "H:i",
+  time_24hr: true,
+  disableMobile: "true"
+})
